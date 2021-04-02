472c9ae21b578da6146875b5fbc4fbae37a4fea7
ritvikverma <ritvik@hku.hk>
2021-04-02 17:58:46 +0800

    linux commit

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from picamera import PiCamera
+from time import sleep
 
+camera = PiCamera()
+camera.rotation = 180
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+camera.start_preview()
+sleep(5)
+camera.stop_preaview()
