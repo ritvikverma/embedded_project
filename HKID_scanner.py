e0572a914baa26287c69a1210e344a86ddfce78c
Ritvik Verma <ritvik.verma@chekk.me>
2021-04-17 17:56:36 +0800

    done?

--- HKID_scanner.py
+++ HKID_scanner.py
@@ -2,46 +2,114 @@ import time
 
 import cv2
 from imutils.video import VideoStream
+from pyzbar import pyzbar
+
+from data_processing import get_vaccine_data, get_triangulated_data
+from ocr_extractor import execute
 
 vs = VideoStream(src=0).start()  # Uncomment this if you are using Webcam
 time.sleep(2)
-i = 8
+countdown_timer = 20
 prev_time = time.time()
+vaccine_data = {}
+hkid_data = ''
+final_results = {'vaccination_date': '12234234',
+                 'vaccine': 'Comirnaty',
+                 'hkid_number': '',
+                 'name': '',
+                 }
+BARCODE_SCAN = 0
+HKID_SCAN = 1
+RESULTS = 2
+mode = BARCODE_SCAN
+
+
+def get_text():
+    return execute('temp.jpg')
+
 
 while True:
     frame = vs.read()
     height, width, channels = frame.shape
-    upper_left = (width // 4, height // 4)
-    bottom_right = (width * 3 // 4, height * 3 // 4)
+    upper_left = (width // 5, height // 5)
+    bottom_right = (width * 4 // 5, height * 4 // 5)
     curr_time = time.time()
 
-    if i > 5:
-        cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 150), thickness=2)
-        cv2.putText(frame, "Please prepare your HKID for scanning", (width // 6, height // 5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+    if mode == RESULTS:
+        if all(list(final_results.values())):
+            cv2.putText(frame, "Welcome to Hong Kong!", (width // 5, height // 7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+            cv2.putText(frame, "Name: " + final_results['name'], (width // 5, height * 2 // 7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+            cv2.putText(frame, "Vaccination Date: " + final_results['vaccination_date'], (width // 5, height * 3 // 7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+            cv2.putText(frame, "Vaccine: " + final_results['vaccine'], (width // 5, height * 4 // 7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+            cv2.putText(frame, "HKID Number: " + final_results['hkid_number'], (width // 5, height * 5 // 7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
 
-    elif 5 >= i > 0:
-        cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 150), thickness=2)
-        cv2.putText(frame, str(i), (width // 4, height // 5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 150), 2)
+        else:
+            cv2.putText(frame, "Verification Failed.", (width // 5, height // 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(frame, "Please proceed to an immigration checkpoint", (width // 5, height // 4),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    elif i == 0:
-        cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 150), thickness=2)
-        cv2.putText(frame, "Capturing HKID...", (width // 6, height // 5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
-        cv2.imwrite("temp.jpg", frame)
 
-    elif i % 3 == 0:
-        cv2.putText(frame, "HKID Captured. Waiting for results...", (width // 4, height // 2),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+    elif mode == HKID_SCAN:
+        if countdown_timer > 5:
+            cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 150), thickness=2)
+            cv2.putText(frame, "Please prepare your HKID for scanning", (width // 6, height // 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
 
-    elif i % 3 == 1:
-        cv2.putText(frame, "HKID Captured. Waiting for results..", (width // 4, height // 2),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+        elif 5 >= countdown_timer > 0:
+            cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 150), thickness=2)
+            cv2.putText(frame, str(countdown_timer), (width // 4, height // 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 150), 2)
 
-    elif i % 3 == 2:
-        cv2.putText(frame, "HKID Captured. Waiting for results.", (width // 4, height // 2),
+        elif countdown_timer == 0:
+            cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 150), thickness=2)
+            cv2.putText(frame, "Capturing HKID...", (width // 6, height // 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+            cv2.imwrite("temp.jpg", frame)
+
+        elif countdown_timer == -1:
+            cv2.putText(frame, "HKID Captured. Waiting for results...", (width // 5, height // 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+            hkid_data = get_text()['raw_text']
+
+        elif countdown_timer == -2:
+            mode = RESULTS
+            final_results = get_triangulated_data(vaccine_data, hkid_data)
+
+    elif vaccine_data and countdown_timer <= 10:
+        mode = HKID_SCAN
+
+    elif not vaccine_data and countdown_timer <= 10:
+        cv2.putText(frame, "ERROR! Please restart the COVID vaccine verification process", (width // 6, height // 5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if countdown_timer < 5:
+            break
+
+    elif mode == BARCODE_SCAN:
+        cv2.putText(frame, "Please present your vaccination QR code", (width // 6, height // 5),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 150), 2)
+        barcodes = pyzbar.decode(frame)
+        for barcode in barcodes:
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            barcodeData = barcode.data.decode("utf-8")
+            try:
+                vaccine_data = get_vaccine_data(barcodeData)
+            except:
+                vaccine_data = {}
+
+            text = vaccine_data['name'] + "(" + vaccine_data['hkid'] + ")"
+            cv2.putText(frame, text, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    if curr_time - prev_time >= 1:
+        countdown_timer -= 1
+        prev_time = curr_time
 
     cv2.imshow("COVID-19 Vaccine Verification System", frame)
     key = cv2.waitKey(1) & 0xFF
@@ -49,10 +117,6 @@ while True:
     # if the `q` key is pressed, break from the loop
     if key == ord("q"):
         break
-
-    if curr_time - prev_time >= 1:
-        i -= 1
-        prev_time = curr_time
 
 cv2.destroyAllWindows()
 vs.stop()
---
+++ data_processing.py
@@ -0,0 +1,38 @@
+import re
+
+
+def get_triangulated_data(vaccine_data, hkid_data):
+    results = {'vaccination_date': vaccine_data['date'],
+               'vaccine': " ".join(vaccine_data['vaccine'].split(" ")[:5]),
+               'hkid_number': '',
+               'name': '',
+               }
+
+    hkid_match = r'' + vaccine_data['hkid'].replace("*", '.').replace("(", "\(").replace(")", "\)")
+    hkid = re.findall(hkid_match, hkid_data)
+    if hkid:
+        hkid = hkid[0]
+        results['hkid_number'] = hkid
+
+    name_match = r'' + vaccine_data['name'].replace('*', '.')
+    name = re.findall(name_match, hkid_data)
+    if name:
+        name = name[0]
+        results['name'] = name
+
+    return results
+
+
+def get_vaccine_data(vaccine_raw_data):
+    vaccine_raw_data = vaccine_raw_data.split('|')
+    vaccine_data = {'hkid': vaccine_raw_data[5],
+                    'name': vaccine_raw_data[6],
+                    'date': vaccine_raw_data[7],
+                    'vaccine': vaccine_raw_data[8]}
+    return vaccine_data
+
+
+if __name__ == '__main__':
+    print(get_triangulated_data({'hkid': '****467(A)', 'name': 'VERMA, R*****', 'date': '06-04-2021',
+                                 'vaccine': 'Comirnaty COVID-19 mRNA Vaccine (BNT162b2) Concentrate for Dispersion for Injection'},
+                                'HONG KONG IDENTITY CARD VERMA, Ritvik HI Date of Birth 29-12-1998 CO EM Date of Issue (08-17) 28-08-17 M715467(A) Scanned with CamScanner'))
